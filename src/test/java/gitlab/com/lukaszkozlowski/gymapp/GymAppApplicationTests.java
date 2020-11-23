d9e7d7429746a70f517515eeeea192cb7aa307ae
lukaszk1989 <l.kozlowski17@gmail.com>
2020-11-23 11:17:26 +0100

    Initial commit

---
+++ src/test/java/gitlab/com/lukaszkozlowski/gymapp/GymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gitlab.com.lukaszkozlowski.gymapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
